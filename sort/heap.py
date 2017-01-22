51f13cca08e08feeaa13976dd1a91f49bab8e94d
Jakub Tyniecki <jakubtyniecki@gmail.com>
2017-01-22 15:28:27 +0000

    sorting

--- sort/heap.py
+++ sort/heap.py
@@ -16,33 +16,31 @@ def sort(arr):
 def heapsort(arr):
     """ heap sort """
 
-    # convert arr to heap
+    heapify(arr)
+
+    for i in [x for x in range(len(arr) - 1, 0, -1) if arr[0] > arr[x]]:
+        arr[0], arr[i] = arr[i], arr[0]
+        movedown(arr, 0, i - 1)
+
+def heapify(arr):
+    """ convert arr to heap """
+
     length = len(arr) - 1
     parent = length // 2
 
     for i in range(parent, -1, -1):
         movedown(arr, i, length)
 
-    # flatten heap into sorted array
-    for i in [x for x in range(length, 0, -1) if arr[0] > arr[x]]:
-        arr[0], arr[i] = arr[i], arr[0]
-        movedown(arr, 0, i - 1)
+def movedown(arr, start, end):
+    """ constraints heap """
 
-def movedown(arr, first, last):
-    """ move down """
-
-    largest = 2 * first + 1
-
-    while largest <= last:
-        # right child exists and is larger than left child
-        if largest < last and arr[largest] < arr[largest + 1]:
-            largest += 1
-
-        # right child is larger than parent
-        if arr[largest] > arr[first]:
-            arr[first], arr[largest] = arr[largest], arr[first]
-            # move down to largest child
-            first = largest
-            largest = 2 * first + 1
+    root = start
+    while root * 2 + 1 <= end:
+        child = root * 2 + 1
+        if child + 1 <= end and arr[child] < arr[child + 1]:
+            child += 1
+        if child <= end and arr[root] < arr[child]:
+            arr[root], arr[child] = arr[child], arr[root]
+            root = child
         else:
             return

04bfae5383148c33c4a55e52cc2bb4c9b2d1e271
Jakub Tyniecki <jakubtyniecki@gmail.com>
2017-02-01 20:42:53 +0000

    plot

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """ main """
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 from perf import metrics
 from sort import base, insert, shell, merge_rec, merge, heap, quick_rec, quick, hybrid_rec, hybrid
 
-import random
+#import random
 
 def main():
     """ main """
@@ -16,20 +19,44 @@ def main():
 
     results = {}
 
-    results["base"] = metrics.execute(base)
-    results["insert"] = metrics.execute(insert)
-    results["shell"] = metrics.execute(shell)
-    results["merge (rec)"] = metrics.execute(merge_rec)
-    results["merge"] = metrics.execute(merge)
-    results["heap"] = metrics.execute(heap)
-    results["quick (rec)"] = metrics.execute(quick_rec)
-    results["quick"] = metrics.execute(quick)
-    results["hybrid (rec)"] = metrics.execute(hybrid_rec)
-    results["hybrid"] = metrics.execute(hybrid)
+    results["base"] = metrics.execute_short(base)
+    results["insert"] = metrics.execute_short(insert)
+    results["shell"] = metrics.execute_short(shell)
+    results["merge (rec)"] = metrics.execute_short(merge_rec)
+    results["merge"] = metrics.execute_short(merge)
+    results["heap"] = metrics.execute_short(heap)
+    results["quick (rec)"] = metrics.execute_short(quick_rec)
+    results["quick"] = metrics.execute_short(quick)
+    results["hybrid (rec)"] = metrics.execute_short(hybrid_rec)
+    results["hybrid"] = metrics.execute_short(hybrid)
 
     print("{name: <15}\t{}\t{}\t{}".format("small", "medium", "large", name=""))
     for key in sorted(results.keys()):
         print("{name: <15}\t{}\t{}\t{}".format(\
             results[key]["small"], results[key]["medium"], results[key]["large"], name=key))
+
+    results["base"] = metrics.execute_long(base)
+    results["insert"] = metrics.execute_long(insert)
+    results["shell"] = metrics.execute_long(shell)
+    results["merge (rec)"] = metrics.execute_long(merge_rec)
+    results["merge"] = metrics.execute_long(merge)
+    results["heap"] = metrics.execute_long(heap)
+    results["quick (rec)"] = metrics.execute_long(quick_rec)
+    results["quick"] = metrics.execute_long(quick)
+    results["hybrid (rec)"] = metrics.execute_long(hybrid_rec)
+    results["hybrid"] = metrics.execute_long(hybrid)
+
+    exes = [x for x in metrics.gen(metrics.TESTS_LONG_NUM)]
+    fig, ax = plt.subplots()
+    #ax.set_xscale('log', basex=2)
+    ax.set_yscale('log', basey=2)
+    for key in sorted(results.keys()):
+        plt.plot(exes, results[key], label=key)
+
+    plt.title("Comparison of sorting algorithms")
+    plt.xlabel("number of elements")
+    plt.ylabel("time elapsed (seconds)")
+    plt.legend(loc='upper left')
+    plt.show()
 
 main()
--- perf/metrics.py
+++ perf/metrics.py
@@ -3,19 +3,37 @@
 import random
 import timeit
 
-TESTS_COUNT = 10
-TESTS = {
+TESTS_SHORT_COUNT = 10
+TESTS_SHORT = {
     "small": 10,
     "medium": 300,
     "large": 1500
 }
 
-def execute(sut):
+def execute_short(sut):
     """ execute """
     results = {}
-    for key in TESTS.keys():
-        test_arr = random.sample(range(TESTS[key]), TESTS[key])
+    for key in TESTS_SHORT.keys():
+        test_arr = random.sample(range(TESTS_SHORT[key]), TESTS_SHORT[key])
         action = lambda arr=test_arr: sut.sort(arr[:])
-        results[key] = "{:0.5f}".format(timeit.timeit(action, number=TESTS_COUNT))
+        results[key] = "{:0.5f}".format(timeit.timeit(action, number=TESTS_SHORT_COUNT))
     return results
 
+TESTS_LONG_COUNT = 1
+TESTS_LONG_NUM = 8
+
+def gen(val):
+    """ gen """
+    i = 8
+    for current in range(val + 1):
+        yield i
+        i <<= 1
+
+def execute_long(sut):
+    """ execute """
+    results = []
+    for num in gen(TESTS_LONG_NUM):
+        test_arr = random.sample(range(num), num)
+        action = lambda arr=test_arr: sut.sort(arr[:])
+        results.append(timeit.timeit(action, number=TESTS_LONG_COUNT) * 10)
+    return results

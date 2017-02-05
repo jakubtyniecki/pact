93bc6860d5771ee7f7e743beba2ad45132a9c052
Jakub Tyniecki <jakubtyniecki@gmail.com>
2017-02-05 14:49:12 +0000

    refactor

--- sort/framework.py
+++ sort/framework.py
@@ -3,8 +3,10 @@
 
 def validate(func):
     """ verify """
+
     def inner(arr):
         """ inner """
+
         if arr is None:
             raise TypeError("'NoneType' object is not iterable")
         if not arr:
--- tests/helper.py
+++ tests/helper.py
@@ -3,4 +3,5 @@
 
 def is_sorted(arr):
     """ assert array is sorted """
+
     return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
